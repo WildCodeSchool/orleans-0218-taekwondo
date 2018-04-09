d7e5061cdaf844614f7d88b5864064977c62b228
EmilieBRG <emibgr@gmail.com>
2018-04-09 16:16:49 +0200

    modification HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,12 +12,6 @@ class HomeController extends AbstractController
 {
     public function index(): string
     {
-        return $this->twig->render('Home/index.html.twig', [
-            'presentation'=> [
-                'image' => 'http://via.placeholder.com/350x150',
-                'title' => 'Présentation du club',
-                'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel augue tempor justo molestie congue. Aliquam quam nisi, vehicula eget metus sed, sollicitudin ultricies mauris.',
-            ]
-        ]);
+        return $this->twig->render('Home/index.html.twig');
     }
 }
