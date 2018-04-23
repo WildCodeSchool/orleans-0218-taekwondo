9fce1a649199095abebe095ea767710dd56aded6
Sébastien <sebstn.hkzt@laposte.net>
2018-04-23 15:54:56 +0200

    Fichiers Data & Export .sql coorespondent respéctivement aux Data et à la structure de la BDD

--- db_schemes/footer/Data.sql
+++ db_schemes/footer/Data.sql
@@ -16,23 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `links`
---
-
-DROP TABLE IF EXISTS `links`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `links` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `web_address` text NOT NULL,
-  `text_of_link` text NOT NULL,
-  `open_in_other_window` int(11) DEFAULT NULL,
-  `link_title` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `links`
 --
 
@@ -51,4 +34,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-23 13:37:57
+-- Dump completed on 2018-04-23 15:49:43
