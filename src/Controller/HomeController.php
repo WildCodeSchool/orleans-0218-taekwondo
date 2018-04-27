9c02b9c85963375ada946de79e7de367e7961753
Sébastien <sebstn.hkzt@laposte.net>
2018-04-27 09:46:04 +0200

    test anti-robot (requiert lignes de script dans le layout, modification du css)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,7 +1,12 @@
 <?php
+
 namespace Controller;
+
 use Model\Event;
 use Model\Footer;
+use Swift_Mailer;
+use Swift_Message;
+use Swift_SmtpTransport;
 
 class HomeController extends AbstractController
 {
@@ -40,48 +45,70 @@ class HomeController extends AbstractController
     public function contactCreate(): void
     {
 
-        // Page de provenance de la requête du formulaire
+        // Adresse de la page de provenance de la requête du formulaire
         $origin = $_SERVER['HTTP_REFERER'];
 
 
+        // Vérification reCaptcha
+
+        // Ma clé privée
+        $secret = "6LfI1FUUAAAAAEgpW-sWlr1DvyAuC02m0KxfS3zi";
+        // Paramètre renvoyé par le recaptcha
+        $response = $_POST['g-recaptcha-response'];
+        // IP de l'utilisateur
+       $remoteip = $_SERVER['REMOTE_ADDR'];
+
+        $api_url = "https://www.google.com/recaptcha/api/siteverify?secret="
+            . $secret
+            . "&response=" . $response
+            . "&remoteip=" . $remoteip ;
+
+        $decode = json_decode(file_get_contents($api_url), true);
+
+
         // Si le formulaire est INcomplet
-        if (empty($_POST['courriel']) || empty($_POST['message'])) {
+        if (empty($_POST['courriel']) || empty($_POST['message']) || ($decode['success'] == false)) {
 
             header("Location: $origin");
 
 
-            // Quand le formulaire est complet
-        } else{
-
-            // Utilisation de la librairie SwiftMailer pour la gestion des courriels
-            // Fonctionnement en 4 étapes
-
-            // 1) Create the Transport
-            $transport = (new Swift_SmtpTransport(APP_MAIL_HOST, APP_MAIL_PORT, APP_MAIL_SECURITY))
-                ->setUsername(APP_MAIL_NAME)
-                ->setPassword(APP_MAIL_PWD);
+        // Quand le formulaire est complet
+        } else {
 
 
-            // 2) Create the Mailer using your created Transport
-            $courrier = new Swift_Mailer($transport);
+            // Avec "if", on vérifie que le système reCAPTCHA ai identifié celui qui fait la requête comme un humain
+            // Dans ce cas, le courriel est traité puis envoyé
+            if ($decode['success'] == true) {
+
+                // Utilisation de la librairie SwiftMailer pour la gestion des courriels
+                // Fonctionnement en 4 étapes
+
+                // 1) Create the Transport
+                $transport = (new Swift_SmtpTransport(APP_MAIL_HOST, APP_MAIL_PORT, APP_MAIL_SECURITY))
+                    ->setUsername(APP_MAIL_NAME)
+                    ->setPassword(APP_MAIL_PWD);
 
 
-            // 3) Create a message
-            $contact = (new Swift_Message('Taekwondo Olivet'))
-                ->setTo(['sebstn.hkzt@laposte.net' => 'PO'])
-                ->setBody(trim(strip_tags($_POST['message'])))
-                ->setFrom([trim(strip_tags($_POST['courriel'])) => 'Visiteur du site'])
-                ->setSubject('Taekwondo Olivet');
+                // 2) Create the Mailer using your created Transport
+                $courrier = new Swift_Mailer($transport);
 
 
-            // 4) Send the message
-            $result = $courrier->send($contact);
+                // 3) Create a message
+                $contact = (new Swift_Message('Taekwondo Olivet'))
+                    ->setTo(['sebstn.hkzt@laposte.net' => 'PO'])
+                    ->setBody(trim(strip_tags($_POST['message'])))
+                    ->setFrom([trim(strip_tags($_POST['courriel'])) => 'Visiteur du site'])
+                    ->setSubject('Taekwondo Olivet');
 
 
-            unset($_POST);
-            header("Location: $origin");
+                // 4) Send the message
+                $result = $courrier->send($contact);
+
+
+                unset($_POST);
+                header("Location: $origin");
+            }
         }
-
 
     }
 
