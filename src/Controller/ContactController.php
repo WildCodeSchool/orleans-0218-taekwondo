bf7ebe1a54d011f33f3c7176efbf077978de1c05
EmilieBRG <emibgr@gmail.com>
2018-05-03 17:06:35 +0200

    page contact 2

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -7,12 +7,13 @@
  */
 
 namespace Controller;
+use Model\Footer;
 
 
 class ContactController extends AbstractController
 {
     public function index(): string
     {
-        return $this->twig->render('Contact/index.html.twig');
+        return $this->twig->render('Contact/index.html.twig', ['links' => (new Footer\LinkManager())->getAll(),]);
     }
 }
